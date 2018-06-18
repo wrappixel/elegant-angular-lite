5c2e156f7038f0ca94e8e494bce64848b2316fff
Vishal Bhatt <bvishal157@gmail.com>
2018-06-18 13:04:11 +0530

    ''

---
+++ src/app/layouts/full/full.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+declare var $: any;
+
+@Component({
+  selector: 'app-full-layout',
+  templateUrl: './full.component.html',
+  styleUrls: ['./full.component.scss']
+})
+export class FullComponent implements OnInit {
+
+  constructor(public router: Router) { }
+
+  topOffset = 55;
+  height: any;
+
+  public innerWidth: any;
+  public innerHeight: any;
+  public lockSidebar = false;
+  public showMobileMenu = false;
+  public addMiniSidebar = false;
+  public hideLogoText = false;
+
+  ngOnInit() {
+    if (this.router.url === '/') {
+      this.router.navigate(['/starter']);
+    }
+    this.handleLayout();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.handleLayout();
+  }
+
+  toggleSidebar() {
+    this.showMobileMenu = true;
+  }
+
+  handleLayout() {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth < 1170) {
+      this.addMiniSidebar = true;
+      this.hideLogoText = true;
+    } else {
+      this.addMiniSidebar = false;
+      this.hideLogoText = false;
+    }
+    this.innerHeight = window.innerHeight;
+    this.height = this.innerHeight - this.topOffset;
+    if (this.height < 1) {
+      this.height = 1;
+    }
+    if (this.height > this.topOffset) {
+      this.height = this.height + 'px';
+    }
+  }
+}
